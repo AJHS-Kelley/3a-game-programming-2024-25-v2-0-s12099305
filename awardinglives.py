fab6d972d5118f9affc92f78a42b5716733890cd
s12099305 <marcusrosier77@gmail.com>
2024-09-12 12:55:05 -0400

    Create awardinglives.py

---
+++ awardinglives.py
@@ -0,0 +1,15 @@
+# awarding extra lives, ryan kelly , v0.0
+
+
+lives = 3
+
+# allow the user to input the score as an integer.
+
+# if score is 10000 or less
+# lose a life
+# if score is > 10000 but less than 100001
+ #gives 1 extra life
+# if score is > 100000
+# give 2 extra lives
+
+# output the score and number of lives to the screen
