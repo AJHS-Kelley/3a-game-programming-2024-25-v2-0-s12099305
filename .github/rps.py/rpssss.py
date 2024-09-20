e8b0ac7558cf9c1c66c082ea1d28f62fa27cf996
s12099305 <marcusrosier77@gmail.com>
2024-09-20 13:03:17 -0400

    Create rpssss.py

---
+++ .github/rps.py/rpssss.py
@@ -0,0 +1,131 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+anything in between the sets of double-quotes is just ignored
+
+
+
+
+# main game loop
+while playerscore < 5 and Cpuscore < 5:
+print(f'{marco}') you have {playerscore} points./nThe CPU has {cpuscore} points.n/")
+playerchoice = input (Please eneter rock, paper, or scissors and press eneter./n").lower()
+if player choice != "rock" and playerchoice !="paper" and player choice != "scissors": 
+playerchoice = input ("please enter rock, paper, or scissors and press eneter./").lower()
+
+
+
+
+
+print (f"you have chosen {playerchoice}./n")
+
+# let cpu select choice at random .
+cpuchoice= random.randit(0,2)
+
+# compare player choice to cpu choice
+# print the results to the screen
+# award point to winner and output results.
+elif cpuchoice== 2
+cpuchoice = "paper"
+else:
+print("unable to determine cpu choice./nPlease restart./n")
+exit()
+# 
+
+
+
+print(f"the cpu cg=hose {cpuchoice} and you choose {playerchoice}./n")
+print("the cpu wins a point./n")
+cpuscore += 1
+#cpu wins
+
+
+
+
+
+
+
+
+ # pirnt the results to the screen
+if playerchoice== "rock" and cpuchoice == "paper":
+pass
+# cpu wins
+elif playerchoice== "rock" and cpuchoice == "scissors":
+pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
